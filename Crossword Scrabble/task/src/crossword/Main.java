3f201dadd620f84f2c1760ad50976c5e3120744b
Ahmed Hosny <ahhosnyas@gmail.com>
2023-08-11 16:50:16 +0300

    Stage 1/5: Scrabble tiles

--- Crossword Scrabble/task/src/crossword/Main.java
+++ Crossword Scrabble/task/src/crossword/Main.java
@@ -1,7 +1,25 @@
 package crossword;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.print("Welcome to Crossword Scrabble\nBag: ");
+        try (BufferedReader file = Files.newBufferedReader(Path.of(args[0]))) {
+            file.lines()
+                    .map(l -> l.split(" "))
+                    .filter(l -> l.length == 2 && isNumber(l[1]))
+                    .forEach(l -> System.out.print(l[0].repeat(Math.max(0, Integer.parseInt(l[1])))));
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private static boolean isNumber(String number) {
+        return number.matches("\\d+");
     }
 }
